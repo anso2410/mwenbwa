8f8991f5349837c603a8f306a95ea155aa776b06
as <simana.annesophie@gmail.com>
2020-10-05 17:37:49 +0200

    try to display popup

--- src/client/components/carte.js
+++ src/client/components/carte.js
@@ -1,8 +1,11 @@
+import {Popup} from "leaflet";
+
 import * as React from "react";
 import {Map, Marker, TileLayer} from "react-leaflet";
 import Trees from "../../../data/arbustums02.json";
 
 export default function Test() {
+    const [activeTree, setActiveTree] = React.useState(null);
     return (
         <Map center={[50.632557, 5.579666]} zoom={12}>
             <TileLayer
@@ -16,8 +19,19 @@ export default function Test() {
                 <Marker
                     key={(tree.x_lambert72, tree.y_lambert72, tree.arbotag)}
                     position={[tree.geoloc.lat, tree.geoloc.lon]}
+                    onClick={() => {
+                        setActiveTree(tree);
+                    }}
                 />
             ))}
+            {activeTree && (
+                <Popup
+                    position={[activeTree.geoloc.lat, activeTree.geoloc.lon]}>
+                    <div>
+                        <h2>{activeTree.x_lambert72}</h2>
+                    </div>
+                </Popup>
+            )}
         </Map>
     );
 }
