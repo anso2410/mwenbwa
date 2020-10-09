4620486d95b39e87ec46a7c8136ccf9f57d380da
ldolne <dolnelaetitia@gmail.com>
2020-10-09 13:53:07 +0200

    Lock function

--- src/server/utilities/calculations.js
+++ src/server/utilities/calculations.js
@@ -55,11 +55,6 @@ exports.lockTree = async (req, res, next) => {
     try {
         const treeToLock = await Tree.findById(req.params.id);
 
-        console.log(treeToLock.location);
-
-        console.log(treeToLock.location.coordinates[0]);
-        console.log(treeToLock.location.coordinates[1]);
-
         const surroundingTrees = await Tree.find({
             location: {
                 $near: {
@@ -74,12 +69,43 @@ exports.lockTree = async (req, res, next) => {
                 },
             },
         });
-        console.log(surroundingTrees.length);
-        res.json({surroundingTrees});
+
+        // Value of all surrounding trees
+        let valueOfAllSurroundingTrees = surroundingTrees.reduce((accumulator, tree) => {
+                return accumulator + parseInt(tree.value);
+            }, 0); // surroundingTrees[0].value
+
+
+        // Count of all surrounding users
+        let listOfOwnerIds = [];
+        surroundingTrees.forEach(tree => {
+            if (tree.owner_id != null && !listOfOwnerIds.includes(tree.owner_id))
+            {
+                listOfOwnerIds.push(tree.owner_id);
+            }
+        });
+        let amountOfPlayers = listOfOwnerIds.length;
+
+        /*
+        // Value of all surrounding trees belonging to player
+        let valueOfAllSurroundingTreesOfPlayer = surroundingTrees.reduce((accumulator, tree) => {
+            if (tree.owner_id === treeToLock.owner_id)
+            {
+                accumulator += parseInt(tree.value);
+            }
+
+            return accumulator;
+        }, 0);
+
+        const valueToPayToLockTree = (treeToLock.value * 10) + (valueOfAllSurroundingTrees * amountOfPlayers) -
+            Math.round(valueOfAllSurroundingTreesOfPlayer / amountOfPlayers);*/
+
+        console.log(valueOfAllSurroundingTrees);
+        res.json({"Amount of Players": amountOfPlayers, Length: surroundingTrees.length, Value: valueOfAllSurroundingTrees, Trees: surroundingTrees});
         //res.json({msg: "Ok."});
     } catch (err) {
         console.log({errors: [{msg: "Server internal error."}]});
-        res.status(500).json({error: "Server internal error."});
+        res.status(500).json({ errors: [{ msg: "Server internal error"}]});
     }
 };
 
@@ -89,6 +115,4 @@ Whenever he wants, a player can lock a tree by paying the following formula:
 [value of the tree] × 10 +
 ([value of all the trees in 100m radius] × [amount of players in 100m radius]) -
 ([value of all player's trees in 100m radius] / [amount of players in 100m radius]).
-
-A locked tree can't be buy by another player.
  */
