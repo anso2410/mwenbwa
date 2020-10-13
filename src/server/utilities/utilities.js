821af8f3836dde9be158c51096d05abd1f8ef023
ldolne <dolnelaetitia@gmail.com>
2020-10-13 12:26:52 +0200

    Correct utilities

--- src/server/utilities/utilities.js
+++ src/server/utilities/utilities.js
@@ -33,22 +33,6 @@ exports.testName = async (treeId) => {
     return treeName.given_name;
 };
 
-exports.insertToGamelog = async (logType, userId, treeId, message) => {
-    try {
-        const newLog = new Gamelog({
-            type: logType,
-            user_id: userId,
-            tree_id: treeId,
-            message: message,
-        });
-        await newLog.save();
-
-        console.log({infos: [{msg: "Insertion to gamelog has succeeded."}]});
-    } catch (err) {
-        console.log({errors: [{msg: "Insertion to gamelog has failed."}]});
-    }
-};
-
 exports.buyTree = async (req, res, next) => {
     const userId = req.body.userId;
     const treeId = req.body.treeId;
